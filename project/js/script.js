d7a9b2432771ee0c77b425e003cb8d998876f021
LeibetsederLara <l.leibetseder@students.htl-leonding.ac.at>
2026-04-05 22:41:36 +0200

    level Selection angefangen

--- project/js/script.js
+++ project/js/script.js
@@ -14,18 +14,28 @@ let audioBox = document.querySelectorAll('#audioBox img')[0]
 
 function bgmusic(){
     if(bgmusicBox.style.opacity == '0'){
-        bgmusicBox.style.opacity == '1'
+        bgmusicBox.style.opacity = '1'
     }
     else{
-        bgmusicBox.style.display == '0'
+        bgmusicBox.style.display = '0'
     }
 }
 
 function audio(){
-    if(audioBox.style.opacity == 0){
-        audioBox.style.display == 1
+    if(audioBox.style.opacity == '0'){
+        audioBox.style.display = '1'
     }
     else{
-        audioBox.style.display == 0
+        audioBox.style.display = '0'
     }
+}
+
+function backToMenu(){
+    document.getElementById('levelSelection').style.display = 'none'
+    document.getElementById('startscreen').style.display = 'block'
+}
+
+function loadLevelSelection(){
+    document.getElementById('levelSelection').style.display = 'block'
+    document.getElementById('startscreen').style.display = 'none'
 }
